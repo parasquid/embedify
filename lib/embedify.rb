c6b124e58216f3d703eb69dcce165dae7a938a01
tristan <tristan.gomez@gmail.com>
2012-02-21 01:34:53 +0800

    implement missing attributes

--- lib/embedify.rb
+++ lib/embedify.rb
@@ -12,9 +12,31 @@ module Embedify
     rescue SocketError => e
       raise e
     end
+
+    # check if the opengraph object is complete
+    unless opengraph.valid?
+      # the opengraph object is lacking some mandatory attributes
+      attributes = OpenGraph::Object::MANDATORY_ATTRIBUTES
+      attributes.delete 'url'
+      attributes.each do |attribute|
+        self.send(attribute.to_sym, opengraph)
+      end
+    end
     opengraph
   end
 
   private
+
+  def self.title(document)
+    document['title'] = document[:nokogiri_parsed_document].css('title').first.inner_text
+  end
+
+  def self.type(document)
+    document['type'] = 'website'
+  end
+
+  def self.image(document)
+    puts "image"
+  end
 
 end
